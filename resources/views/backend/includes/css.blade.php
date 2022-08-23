51f78f8985294153f895c7ab9c91cb11c21308e0
asraf-mridha <asrafmridha@gmail.com>
2022-08-23 22:08:50 +0600

    Add Bootstarp5 css cdn

--- resources/views/backend/includes/css.blade.php
+++ resources/views/backend/includes/css.blade.php
@@ -5,6 +5,10 @@
     <link href="{{ asset('backend') }}/lib/select2/css/select2.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" 
      href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+     {{-- bootstarp5 css cdn --}}
+     
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	
     
     <!-- Bracket CSS -->
