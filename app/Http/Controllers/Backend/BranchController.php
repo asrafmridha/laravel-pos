2cfae1f6efe2d9ca43aff4da5a40983324a57198
asraf-mridha <asrafmridha@gmail.com>
2022-08-23 08:15:13 +0600

    Validate for form if user give null value

--- app/Http/Controllers/Backend/BranchController.php
+++ app/Http/Controllers/Backend/BranchController.php
@@ -7,5 +7,24 @@ use Illuminate\Http\Request;
 
 class BranchController extends Controller
 {
-    //
+   
+    function addbranchview(){
+
+        return view("backend.pages.branch.add");
+    }
+
+    function addbranch(Request $request){
+
+       $request->validate([
+
+      'name'=>'required',
+      'manager'=>'required',
+      'phone'=>'required',
+      'email'=>'required|unique:posts|email|max:100', 
+      'status'=>'required',
+
+
+       ]);
+    
+    }
 }
