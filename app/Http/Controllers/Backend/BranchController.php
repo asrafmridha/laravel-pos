2f7c350ecaae25d8a72cfc1adc1d0391fbfa47d7
asraf-mridha <asrafmridha@gmail.com>
2022-08-23 11:23:02 +0600

    Create function for show manage Branch Table and data

--- app/Http/Controllers/Backend/BranchController.php
+++ app/Http/Controllers/Backend/BranchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Branch;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
@@ -20,11 +21,27 @@ class BranchController extends Controller
       'name'=>'required|min:5',
       'manager'=>'required',
       'phone'=>'required',
-      'email'=>'required|email|unique:posts|max:100', 
+      'email'=>'required|email|max:100', 
       'status'=>'required',
 
 
        ]);
+
+       $branch=new Branch;
+       $branch->name=$request->name;
+       $branch->manager=$request->manager;
+       $branch->email=$request->email;
+       $branch->phone=$request->phone;
+       $branch->status=$request->status;
+       $branch->save();
+
+       return redirect()->back()->with('message','Branch information added Successfully');
+
     
+    }
+
+    function managebranch(){
+
+        return view('backend.pages.branch.manage');
     }
 }
