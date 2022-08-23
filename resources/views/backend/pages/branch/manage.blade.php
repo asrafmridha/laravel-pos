eb11a4d8143a56ae7b9d53d8c694f8edd3626681
asraf-mridha <asrafmridha@gmail.com>
2022-08-23 22:13:05 +0600

    Add Bootstarp Modal for Delete Branch

--- resources/views/backend/pages/branch/manage.blade.php
+++ resources/views/backend/pages/branch/manage.blade.php
@@ -20,24 +20,101 @@
                 <td>{{$branch->email}}</td>
                 <td>
                     @if ($branch->status==1)
-                    <a href="{{Route('status'),$branch->id}}" class="btn btn-info btn-sm">Active</a>
+                    <a href="{{Route('status',$branch->id)}}" class="btn btn-info btn-sm">Active</a>
     
                       @else
-                      <a href="{{Route('status'),$branch->id}}" class="btn btn-danger btn-sm">Inactive</a>
+                      <a href="{{Route('status',$branch->id)}}" class="btn btn-danger btn-sm">Inactive</a>
                      
                   @endif
     
                 </td>
                 <td>
                     <a href="" class="btn btn-sm btn-info"><i class="fa fa-edit"></i></a>
-                    <a href="" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
+                    <button data-bs-toggle="modal" data-bs-target="#deletebranchmodal" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
                 </td>
             </tr>
 
             <?php  $sl++; ?>
+
+
+      
+  <!-- Modal For delete Branch-->
+  <div class="modal fade" id="deletebranchmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Confirmation Message</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+      Are you confirm Delete this?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Confirm</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             @endforeach
-           
-            
+
+    
            
        </table>
     @endsection
