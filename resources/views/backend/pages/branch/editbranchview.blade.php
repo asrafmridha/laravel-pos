d99618e21479ee470d00439753cc5a9790d52384
asraf-mridha <asrafmridha@gmail.com>
2022-08-24 07:49:14 +0600

    Create a blade file for update data

---
+++ resources/views/backend/pages/branch/editbranchview.blade.php
@@ -0,0 +1,32 @@
+@extends('backend.mastaring.master')
+    @section('asraf')
+       <div class="col-md-6">
+        {{-- <form action="{{Route('editbranch')}}" method="POST"> --}}
+               
+                    
+              
+                <input value="{{$editbranch->name}}" type="text" name="name" placeholder="Enter Branch Name" class="mt-3 form-control">
+            
+                <input value="{{$editbranch->manager}}" type="text" name="manager" placeholder="Enter Manager Name" class="mt-3 form-control">
+
+                <input value="{{$editbranch->phone}}" type="text" name="phone" placeholder="Enter phone Number" class="mt-3 form-control">
+
+        
+    
+                <input value="{{$editbranch->email}}" type="text" name="email" placeholder="Enter Email" class="mt-3 form-control">
+
+            
+                <select name="status" class="mt-3 form-control">
+                    <option value="{{$editbranch->status}}">-----Select Status-----</option>
+                    <option value="1" >Active</option>
+                    <option value="2"
+                    >Inactive</option>
+                </select>
+               
+                <button type="submit" class="mt-3 btn btn-success form-control">Edit Branch</button>
+            </form>
+       </div>
+    @endsection
+
+    
+        
