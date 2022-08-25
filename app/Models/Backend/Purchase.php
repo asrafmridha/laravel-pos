d142aa75f2c7226c46e8d570ed8adac4e9f993ee
asraf-mridha <asrafmridha@gmail.com>
2022-08-25 23:09:01 +0600

    Protected fillable field in Purchases Model

--- app/Models/Backend/Purchase.php
+++ app/Models/Backend/Purchase.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Purchase extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+
+        'date',
+        'branch_id',
+        'company_name',
+        'invoice',
+        'product_id',
+        'discount',
+        'discount_ammount',
+        'total_ammount'
+
+
+
+
+    ];
 }
