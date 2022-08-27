2a5ed3b1b83b86990132a6b4524cb65fa4701618
asraf-mridha <asrafmridha@gmail.com>
2022-08-28 00:00:57 +0600

    Design for ProductReport blade

---
+++ resources/views/backend/pages/stock/productreport.blade.php
@@ -0,0 +1,32 @@
+@extends('backend.mastaring.master')
+    @section('asraf')
+
+    <table class="table">
+        <tr>
+            <th>#sl no</th>
+            <th>Branch Name</th>
+            <th>Branch Manager Name</th>
+            <th>Cost_Price</th>
+            <th>Sale_Price</th>
+            <th>Quantity</th>
+          
+        </tr>
+
+     
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            
+           
+        </tr>
+    
+  
+        
+
+   </table>
+       
+    @endsection
